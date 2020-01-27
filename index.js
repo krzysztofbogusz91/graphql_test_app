b397456ba36c557de3185b9a2fecd13f3b568ccc
Krzysztof Bogusz <krzysztof.bogusz@netguru.com>
2020-01-27 20:26:30 +0100

    Start working on gql schema

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema.js')
 
-const PORT = process.env.PORT || 5000
-
+const app = express();
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
 }))
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT), () => console.log('server started on port ', PORT);
